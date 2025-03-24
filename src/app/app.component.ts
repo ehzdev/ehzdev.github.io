93d7a60624c8e454c16d918db919551aa83f3f2e
ehzdev <ehz.dev@gmail.com>
2025-03-23 22:59:15 -0600

    Componentes compartidos y página Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PreloaderComponent } from './shared/preloader/preloader.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ScrolltopComponent } from './shared/scrolltop/scrolltop.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PreloaderComponent, HeaderComponent, FooterComponent, ScrolltopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/scrolltop/scrolltop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scrolltop',
+  imports: [],
+  templateUrl: './scrolltop.component.html',
+  styleUrl: './scrolltop.component.css'
+})
+export class ScrolltopComponent {
+
+}
