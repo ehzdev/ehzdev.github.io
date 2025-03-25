eb401fa83b16b081d49301bf680f946e8bfffc25
Edgar Haro <ehz.dev@gmail.com>
2025-03-24 18:44:15 -0600

    Componentes pagina Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ExperienceComponent, ServicesComponent, ContactComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Bienvenido a Mi Página Web');
+    this.metaService.updateTag({ name: 'description', content: 'Descripción de la página principal.' });
+  }
 }
