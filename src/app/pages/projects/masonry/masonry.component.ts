1ee555f1c10f05a948087b50e7a9d8479b99b529
Edgar Haro <ehz.dev@gmail.com>
2025-03-27 18:21:32 -0600

    Nueva pagina de proyectos

---
+++ src/app/pages/projects/masonry/masonry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masonry',
+  imports: [],
+  templateUrl: './masonry.component.html',
+  styleUrl: './masonry.component.css'
+})
+export class MasonryComponent {
+
+}
