35a9ceef2f7b809fada938d2479ceed59c295514
Edgar Haro <ehz.dev@gmail.com>
2025-04-04 18:13:39 -0600

    componente header cambio a navbar y ahora es compartido, componente header de pagina cambio a compartido en toda la aplicación

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HeaderService } from '../../shared/header/services/header.service';
 import { Title, Meta } from '@angular/platform-browser';
-import { HeaderComponent } from './header/header.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ServicesComponent } from './services/services.component';
-
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, ExperienceComponent, ServicesComponent],
+  imports: [ExperienceComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor(private titleService: Title, private metaService: Meta) {}
+  constructor(private headerService: HeaderService, private titleService: Title, private metaService: Meta) {}
 
   ngOnInit() {
     this.titleService.setTitle('Bienvenido a Mi Página Web');
     this.metaService.updateTag({ name: 'description', content: 'Descripción de la página principal.' });
+    this.headerService.toggleHeader(true);
+  }
+
+  ngOnDestroy(): void {
+    this.headerService.toggleHeader(false);
   }
 }
